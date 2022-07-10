79690d428aaaa52cf773173317fb2ce5d7aced8c
sssiott <rpkrm25@gmail.com>
2022-07-10 21:08:18 +0900

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `First Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `sssiott`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
