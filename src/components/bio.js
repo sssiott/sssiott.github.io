264b0755382d48c6cdda11f37a1848a98580baf7
sssiott <rpkrm25@gmail.com>
2022-07-14 22:27:22 +0900

    icon fix

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/j_g.png"
+        src="../images/hehe.png"
         width={50}
         height={50}
         quality={95}
